903b148af906509bd85350e368584ae34219979e
Codemartino <akudugumartin5@gmail.com>
2024-02-08 22:55:46 +0000

    will come back

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
-"""
-Password Encryption and Validation Module
-"""
+""" Encrypting pswds with bcrypt """
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
+    """ Takes in string arg, converts to unicode
+    Returns salted, hashed pswd as bytestring
     """
-
-    hashes the password it takes as params using bcrypt
-    """
-    encode = password.encode()
-    hashed = bcrypt.hashpw(encode, bcrypt.gensalt())
-
-    return hashed
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks if hashed and unhashed pswds are same
+    Returns bool
     """
-    checks if hash is valid
-    """
-
-    valid_password = False
-    encode = password.encode()
-    if bcrypt.checkpw(encode, hashed_password):
-        valid_password = True
-    return valid_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
